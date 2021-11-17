e5cac6a24dfd5fc6fc1e3e663d72414ad0428b1d
Abdulai <jalloha92@gmail.com>
2021-11-17 17:21:08 -0500

    Instructor page started 40% done

--- src/shared/Instructors.js
+++ src/shared/Instructors.js
@@ -1,40 +1,46 @@
 export const INSTRUCTORS = [
   {
     id: 0,
-    name: "Features Name 1",
+    name: "Imam Ibrahim",
     image: "/assets/img/Logo.png",
 
-    description: "Arg",
+    description:
+      "Tellus in metus vulputate eu scelerisque felis. Neque vitae tempus quam pellentesque nec nam. Enim sit amet venenatis urna. Massa eget egestas purus viverra accumsan in nisl nisi. Morbi tristique senectus et netus et malesuada fames ac turpis. Accumsan in nisl nisi scelerisque eu ultrices. Enim diam vulputate ut pharetra. Tempor nec feugiat nisl pretium fusce id. Euismod lacinia at quis risus sed. Ac ut consequat semper viverra nam. Dignissim suspendisse in est ante. Risus nec feugiat in fermentum posuere urna. Rhoncus dolor purus non.semper viverra nam. ",
   },
 
   {
     id: 1,
-    name: "Features Name 2",
+    name: "Mamadou Saliou",
     image: "/assets/img/Logo.png",
-    description: "First",
+    description:
+      "A arcu cursus vitae congue. Amet cursus sit amet dictum sit amet justo. Vitae nunc sed velit dignissim. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt. Nibh venenatis cras sed felis eget velit. Etiam erat velit scelerisque in dictum non consectetur. Maecenas accumsan lacus vel facilisis volutpat est velit egestas dui. Cras fermentum odio eu feugiat. In eu mi bibendum neque egestas congue. Aliquam vestibulum morbi blandit cursus. Neque vitae tempus quam pellentesque nec nam aliquam sem et. Quam nulla porttitor massa id neque aliquam ",
   },
   {
     id: 2,
-    name: "Features Name 3",
+    name: "Fatoumata Diaraye",
     image: "/assets/img/Logo.png",
-    description: "Second",
+    description:
+      "Vel facilisis volutpat est velit egestas dui id. Lobortis scelerisque fermentum dui faucibus in ornare. Ac turpis egestas maecenas pharetra. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Consequat ac felis donec et. At varius vel pharetra vel turpis nunc. Nunc consequat interdum varius sit amet. Consequat interdum varius sit amet mattis. Egestas diam in arcu cursus euismod quis. Lorem dolor sed viverra ipsum nunc aliquet bibendum enim facilisis. Maecenas ultricies mi eget mauris. Ultrices sagittis orci a scelerisque purus. vestibulum morbi.",
   },
   {
     id: 3,
-    name: "Features Name 4",
+    name: "Ibrahime",
     image: "/assets/img/Logo.png",
-    description: "what",
+    description:
+      "Eget egestas purus viverra accumsan in nisl nisi scelerisque. In eu mi bibendum neque egestas. Ultricies mi quis hendrerit dolor magna eget est lorem. Morbi tristique senectus et netus et malesuada fames ac. In vitae turpis massa sed elementum tempus. Neque ornare aenean euismod elementum. Congue quisque egestas diam in arcu cursus. Lacinia at quis risus sed vulputate odio ut enim. Cursus vitae congue mauris rhoncus. Magnis dis parturient montes nascetur ridiculus mus mauris. Justo donec enim diam vulputate ut pharetra. Faucibus.",
   },
   {
     id: 4,
-    name: "Features Name 5",
+    name: "Mamadou Cherif",
     image: "/assets/img/Logo.png",
-    description: "whe",
+    description:
+      "Sit amet cursus sit amet dictum sit amet justo donec. Vulputate ut pharetra sit amet aliquam id. Lacinia quis vel eros donec ac odio. Sollicitudin nibh sit amet commodo nulla. Vel orci porta non pulvinar. Amet justo donec enim diam vulputate ut pharetra. Euismod nisi porta lorem mollis. Gravida neque convallis a cras semper. Aliquam etiam erat velit scelerisque. Lacus luctus accumsan tortor posuere ac ut consequat semper. Hendrerit dolor magna eget est lorem. Morbi tristique senectus et netus et malesuada fames ac. Aliquam vestibulum.",
   },
   {
     id: 5,
-    name: "Features Name 6",
+    name: "Taha",
     image: "/assets/img/Logo.png",
-    description: "Brief Feature Introduction goes here",
+    description:
+      "Neque volutpat ac tincidunt vitae semper quis. Id volutpat lacus laoreet non curabitur. Id diam maecenas ultricies mi eget. Augue ut lectus arcu bibendum at varius vel. Libero justo laoreet sit amet cursus sit amet dictum sit. Dignissim cras tincidunt lobortis feugiat vivamus. Metus vulputate eu scelerisque felis imperdiet proin. At imperdiet dui accumsan sit amet. Urna nec tincidunt praesent semper feugiat nibh sed pulvinar proin. Scelerisque varius morbi enim nunc faucibus a pellentesque. Turpis egestas integer eget aliquet. Scelerisque varius morbi enim nunc faucibus",
   },
 ];
